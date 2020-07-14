72954e22debbcaa7de44914c26c1bcd0dad62087
Shubham Patel <srpatel980@gmail.com>
2020-07-14 23:37:02 +0530

    Create download_youtube_videos.py

---
+++ Python Tricks/download_youtube_videos.py
@@ -0,0 +1,12 @@
+# pip uninstall pytube
+# pip uninstall pytube3
+# pip install pytube3
+
+import pytube
+from pytube.cli import on_progress
+
+url = input("Enter video url: ")
+
+yt = pytube.YouTube(url, on_progress_callback=on_progress)
+yt = yt.streams[0].download()
+
