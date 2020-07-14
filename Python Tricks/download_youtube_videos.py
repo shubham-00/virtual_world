396c3a875574dc6b8454243719a399422ce7db8c
Shubham Patel <srpatel980@gmail.com>
2020-07-14 23:38:52 +0530

    Update download_youtube_videos.py

--- Python Tricks/download_youtube_videos.py
+++ Python Tricks/download_youtube_videos.py
@@ -8,5 +8,6 @@ from pytube.cli import on_progress
 url = input("Enter video url: ")
 
 yt = pytube.YouTube(url, on_progress_callback=on_progress)
-yt = yt.streams[0].download()
+# yt = yt.streams[0].download()
+yt.streams.get_highest_resolution().download()
 
